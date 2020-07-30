f638c1c50d6e98a06c3b19d9939118ad62758c6e
THARAKESHWARAN <tharakeshwaran978@gmail.com>
2020-07-30 13:59:59 +0530

    Created API calls and scoring system

--- Pig.js
+++ Pig.js
@@ -13,7 +13,7 @@ class Pig extends BaseClass {
    else{
      World.remove(world, this.body);
      push();
-     this.Visiblity = this.Visiblity - 5;
+     this.Visiblity = this.Visiblity - 10;
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
      pop();
@@ -21,6 +21,10 @@ class Pig extends BaseClass {
    
  }
 
-
+  score(){
+    if(this.Visiblity < 0 && this.Visiblity > -1005){
+      score = score + 1;
+    }
+  }
 
 };
